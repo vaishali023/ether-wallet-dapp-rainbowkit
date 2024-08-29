152fb7378d53dfdebbd25fd9e816dbec70b26c7c
vaishali023 <vaishalichaudhary011@gmail.com>
2024-08-29 15:55:05 -0400

    Project setup and dependencies installed

--- ether-wallet-dapp-rainbowkit/hardhat.config.js
+++ ether-wallet-dapp-rainbowkit/hardhat.config.js
@@ -1,6 +1,35 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const { task } = require('hardhat/config')
+task(
+  'accounts',
+  'Prints the list of accounts and their balances',
+  async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
+
+    for (const account of accounts) {
+      const balance = await account.getBalance()
+      console.log(account.address, ': ', balance);
+    }
+  }
+)
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  paths: {
+    sources: './contracts',
+    artifacts: './src/artifacts'
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    goerli: {
+      url: 'https://goerli-testnet-node-url.com',
+            //accounts: [privateKey1, privateKey2]
+    }
+  }
 };
